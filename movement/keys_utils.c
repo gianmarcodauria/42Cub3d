3b672aeea7a65045166a26ea6452cb70cc0bca92
Christian Calabro <ccalabro@e3r9p7.42roma.it>
2025-05-06 16:08:44 +0200

    pulito keys.c

--- movement/keys_utils.c
+++ movement/keys_utils.c
@@ -1,43 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   keys.c                                             :+:      :+:    :+:   */
+/*   keys_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccalabro <ccalabro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/28 17:33:04 by ccalabro          #+#    #+#             */
-/*   Updated: 2025/05/06 16:03:39 by ccalabro         ###   ########.fr       */
+/*   Created: 2025/05/06 16:03:12 by ccalabro          #+#    #+#             */
+/*   Updated: 2025/05/06 16:03:31 by ccalabro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	key_release_position(int keycode, t_cube *cube)
+static void	key_press_position(int keycode, t_cube *cube)
 {
 	if (keycode == WW)
-		cube->player.move.w = 0;
+		cube->player.move.w = 1;
 	if (keycode == SS)
-		cube->player.move.s = 0;
+		cube->player.move.s = 1;
 	if (keycode == AA)
-		cube->player.move.a = 0;
+		cube->player.move.a = 1;
 	if (keycode == DD)
-		cube->player.move.d = 0;
+		cube->player.move.d = 1;
 }
 
-static void	key_release_rotation(int keycode, t_cube *cube)
+static void	key_press_rotation(int keycode, t_cube *cube)
 {
 	if (keycode == RIGHT)
-		cube->player.rotate_right = 0;
+		cube->player.rotate_right = 1;
 	if (keycode == LEFT)
-		cube->player.rotate_left = 0;
+		cube->player.rotate_left = 1;
 }
 
-int	key_release(int keycode, void *param)
+int	key_press(int keycode, void *param)
 {
 	t_cube	*cube;
 
 	cube = (t_cube *)param;
-	key_release_position(keycode, cube);
-	key_release_rotation(keycode, cube);
+	if (keycode == ESC)
+		destroy(cube);
+	key_press_position(keycode, cube);
+	key_press_rotation(keycode, cube);
 	return (0);
 }
--- movement/update.c
+++ movement/update.c
@@ -6,7 +6,7 @@
 /*   By: ccalabro <ccalabro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 17:28:58 by ccalabro          #+#    #+#             */
-/*   Updated: 2025/05/06 15:54:48 by ccalabro         ###   ########.fr       */
+/*   Updated: 2025/05/06 16:02:46 by ccalabro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,39 +101,3 @@ int	set_rotation(t_cube *cube)
 	}
 	return (has_rotated);
 }
-
-// int	update_position(void *param)
-// {
-// 	t_cube	*cube;
-// 	int		ret;
-
-// 	cube = (t_cube *)param;
-// 	ret = moving(cube);
-// 	return (ret);
-// }
-
-// int	update_rotation(void *param)
-// {
-// 	t_cube	*cube;
-// 	int		has_rotated;
-
-// 	cube = (t_cube *)param;
-// 	has_rotated = 0;
-// 	has_rotated = set_rotation(cube);
-// 	cube->player.perpendicular_direction = cube->player.direction - (PI_G / 2);
-// 	cube->player.fov.half_left = cube->player.direction - (FOV_ANGLE / 2);
-// 	cube->player.fov.half_right = cube->player.direction + (FOV_ANGLE / 2);
-// 	return (has_rotated);
-// }
-
-// int	update_movement(void *param)
-// {
-// 	int	pos_updated;
-// 	int	rot_updated;
-
-// 	pos_updated = update_position(param);
-// 	rot_updated = update_rotation(param);
-// 	if (pos_updated || rot_updated)
-// 		drawing_routine((t_cube *)param);
-// 	return (0);
-// }
